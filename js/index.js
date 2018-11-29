48a2ccb78866dc4db52d3341406789e8601e53e1
lty <499254192@qq.com>
2018-11-29 11:12:01 +0800

    meiyouxinxi

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('你可以使用么?');
+var bg = document.getElementById('background');
+// linear-gradient(160deg, #ff0064 20%,#ca00ff 80%);
+var deg = 160;
+setInterval(() => {
+  console.log(bg)
+  if (deg <= 360) {
+    deg++
+  } else {
+    deg = 160
+  }
+  bg.style.background="linear-gradient(+" + deg + "deg+,#ff006420% ,#ca00ff 80%)";
+}, 1)
