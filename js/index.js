7fa1fcbfaa2dd96fbe36f277e0cf35b5125dbd81
lty <499254192@qq.com>
2018-11-29 15:45:32 +0800

    取消丑陋的横向变形

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ function starObj(){
       var h2 = this.getHeight();
       ctx.fillRect(w2, h2, 2, 2);
       this.speedy.push(Math.random() * (3 - 1) + 1);
-      this.speedx.push((Math.random()-Math.random())* (3 - 1) + 1);
+      // this.speedx.push((Math.random()-Math.random())* (3 - 1) + 1);
       this.widthBOX.push(w2);
       this.heightBOX.push(h2);
     }
@@ -32,15 +32,15 @@ function starObj(){
     ctx.clearRect(0,0,w,h);
     for (var a=0;a<this.maxStars;a++){
       this.heightBOX[a]=this.heightBOX[a]-this.speedy[a];
-      this.widthBOX[a]=this.widthBOX[a]+this.speedx[a];
+      // this.widthBOX[a]=this.widthBOX[a]+this.speedx[a];
       if (this.heightBOX[a] <= 0){
         this.widthBOX[a]=this.getwidth();
         this.heightBOX[a]=this.getHeight();
       }
-      if (this.widthBOX[a]>=w||this.widthBOX[a]<= 0){
-        this.widthBOX[a]=this.getwidth();
-        this.heightBOX[a]=this.getHeight();
-      }
+      // if (this.widthBOX[a]>=w||this.widthBOX[a]<= 0){
+      //   this.widthBOX[a]=this.getwidth();
+      //   this.heightBOX[a]=this.getHeight();
+      // }
       ctx.fillRect(this.widthBOX[a],this.heightBOX[a],2,2)
     }
   }
